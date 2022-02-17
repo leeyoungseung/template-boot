10a015b8c4c94507d0ba1ea97cc658a21d6641e6
leeyoungseung <rudnfskf2@gmail.com>
2022-02-17 23:23:55 +0900

    Hello World

---
+++ src/main/java/com/boot/template/TemplateBootApplication.java
@@ -0,0 +1,13 @@
+package com.boot.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/boot/template/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.boot.template.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(path="/")
+	public String index() {
+		return "index";
+	}
+}
